ee2a22794d9d17e2fdef06a59da52095fdf56452
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-02 10:52:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,34 +5,5 @@ require 'interactive_record.rb'
 class Student < InteractiveRecord
 
   
-  def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
-  end
-
-
-  def self.table_name
-    self.to_s.downcase.pluralize
-  end
-
-  def table_name_for_insert
-    self.class.table_name
-  end
-  
-  def self.column_names
-    DB[:conn].results_as_hash = true
-    sql = "PRAGMA table_info('#{table_name}')"
-    table_info = DB[:conn].execute(sql)
-    column_names = []
-    table_info.each do |column|
-    column_names << column["name"]
-      end
-    column_names.compact #gits rid of nil values 
-  end
-
-  def initialize(options={})
-    options.each do |property, value|
-    self.send("#{property}=", value)
-    end
-  end
 
 end
